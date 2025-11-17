9a47512da0f288f1c2c5182f910f963bfecf1794
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-11-17 10:58:32 -0500

    Initialized backend and frontend. Added folders with mostly empty files to plan out the project. Added git ignores to prevent node_modules and .env files from being pushed to repo.

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import connectDB from "./db/conn.js";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello world!"})
+});
+
+app.listen(port, () => {
+    console.log("Listening on port:", port);
+    connectDB();
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <>
+            <h1>This is my PokeGuesser App.</h1>
+        </>
+    );
+}
