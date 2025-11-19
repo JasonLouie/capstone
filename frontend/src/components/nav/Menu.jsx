190e792375f9ae8a31a654adf49334545b0e9789
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-11-19 00:56:41 -0500

    Added more menu styling and finished implementing logic to hide/show the user menu when logged in.

--- frontend/src/components/nav/Menu.jsx
+++ frontend/src/components/nav/Menu.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../Button";
 import defaultAvatar from "/images/default-avatar.png";
 import Image from "../Image";
@@ -6,13 +6,27 @@ import Image from "../Image";
 export default function Menu() {
     const user = {};
     const [hidden, setHidden] = useState(true);
+    const divRef = useRef(null);
+
+    // Close the menu when clicking anywhere else on the page
+    const closeMenu = (e) => {
+        if (divRef.current && !divRef.current.contains(e.target)) setHidden(true);
+    }
+
+    useEffect(() => {
+        if (!hidden && divRef.current) document.body.addEventListener("click", closeMenu);
+        else document.body.removeEventListener("click", closeMenu);
+        // Clean up for rendering different page without clicking on a link/button
+        return () => document.body.removeEventListener("click", closeMenu);
+    }, [hidden]);
 
     return (
-        <div className="menu-container">
+        <div className="menu-container" onClick={() => setHidden(!hidden) } ref={divRef}>
             <Button className="show-menu-btn"><Image src={user?.profilePicUrl ? user.profilePicUrl : defaultAvatar} alt="User's profile picture" size="smaller" /></Button>
-            <div className="menu">
-                <Button className="menu-item">Profile</Button>
-                <Button className="menu-item">Settings</Button>
+            <div inert={hidden} className={`menu${hidden ? " hidden" : ""}`}>
+                <Button className="menu-item" buttonType="button" >Profile</Button>
+                <Button className="menu-item" buttonType="button" >Settings</Button>
+                <Button className="menu-item" buttonType="button" >Logout</Button>
             </div>
         </div>
     );
